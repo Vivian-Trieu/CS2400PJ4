775d91b24b4d544c71d83931daa89c44f7ba2daf
Vivian Trieu <vdtrieu@cpp.edu>
2021-04-12 19:54:05 -0700

    Add reheap method from textbook

--- MaxHeap.java
+++ MaxHeap.java
@@ -88,6 +88,35 @@ public final class MaxHeap<T extends Comparable<? super T>>
         lastIndex = 0;
     } // end clear
    
-// Private methods
+    // Precondition: checkIntegrity has been called.
+    private void reheap(int rootIndex)
+    {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+
+        while (!done && (leftChildIndex <= lastIndex) )
+        {
+            int largerChildIndex = leftChildIndex; // Assume larger
+            int rightChildIndex = leftChildIndex + 1;
+
+            if ( (rightChildIndex <= lastIndex) &&
+                heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+            {
+                largerChildIndex = rightChildIndex;
+            } // end if
+
+            if (orphan.compareTo(heap[largerChildIndex]) < 0)
+            {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+            }
+            else
+                done = true;
+        } // end while
+
+        heap[rootIndex] = orphan;
+    } // end reheap
 // . . .
 } // end MaxHeap
